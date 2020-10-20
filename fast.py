2c0d0ba9afa7e31384b97dfb264a2c1ee8105702
abhishek193 <72853952+abhishek193@users.noreply.github.com>
2020-10-20 19:04:12 +0530

    Add files via upload
    updated

--- fast.py
+++ fast.py
@@ -4,8 +4,8 @@ print(fastai.__version__)
 from fastai.vision.all import *
 
 classes = ['badminton', 'tennis']
-path = Path('fast')
 
+'''
 def getdata():
     for class_name in classes:
         folder = class_name
@@ -13,14 +13,12 @@ def getdata():
         dest = path/folder
         dest.mkdir(parents=True, exist_ok=True)
         download_images(path/file, dest, max_pics=200)
-
+'''
 def label_func(f): 
   return f[0] == 't'
 
-cd train/
-
 import os
-files = os.listdir(os.getcwd())
+files = os.listdir(os.getcwd()+'/train')
 
 def train(files):
     dls = ImageDataLoaders.from_path_func(os.getcwd(), files, label_func, item_tfms=Resize(100))
@@ -29,9 +27,7 @@ def train(files):
     return learn
 
 
-
 #getdata()
 my_model = train(files)
 
-
-
+my_model.save('my_model')
