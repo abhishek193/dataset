736b49d70ce4d7415a7dc6d4405b9fa31d625399
abhishek193 <72853952+abhishek193@users.noreply.github.com>
2020-10-21 09:47:33 +0530

    Add files via upload
    update

--- test.py
+++ test.py
@@ -3,12 +3,12 @@ print(fastai.__version__)
 
 from fastai.vision.all import *
 import sys
+import fast
 
 def predict(model, image):
     out = model.predict(image)
     return out
 
-my_model = learn.load_learner('my_model')
 
 out = predict(my_model, '/test10/test/1.jpg')
 print(out)
