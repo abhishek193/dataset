4ab455c5a38881df22556ddd6eef6898b434aadc
abhishek193 <72853952+abhishek193@users.noreply.github.com>
2020-10-21 12:17:26 +0530

    Update test.py

--- test.py
+++ test.py
@@ -12,5 +12,5 @@ def predict(model, image):
 
 path = os.getcwd()+'/test10/test'
 os.chdir('/dataset/test10/test')
-#out = predict(fast.my_model, '1.jpg')
-#print(out)
+out = predict(fast.my_model, '1.jpg')
+print(out)
