c540889c65d9c079a97fd88644541ad2d658d48d
abhishek193 <72853952+abhishek193@users.noreply.github.com>
2020-10-21 11:05:43 +0530

    Add files via upload

--- fast.py
+++ fast.py
@@ -18,19 +18,17 @@ def label_func(f):
   return f[0] == 't'
 
 import os
-files = os.listdir(os.getcwd()+'/train50')
+files = os.listdir(os.getcwd()+'/train50/train')
 path = os.getcwd() + '/train50/train'
-print(len(files))
-print(os.getcwd())
-print(path)
+
 
 def train(files):
-    dls = ImageDataLoaders.from_path_func(os.getcwd(), files, label_func, item_tfms=Resize(100))
+    dls = ImageDataLoaders.from_path_func(os.getcwd()+'train50/train', files, label_func, item_tfms=Resize(100))
     learn = cnn_learner(dls, resnet34, metrics=error_rate)
     learn.fine_tune(1)
     return learn
 
 #getdata()
-#my_model = train(files)
+my_model = train(files)
 
 #my_model.save('my_model')
